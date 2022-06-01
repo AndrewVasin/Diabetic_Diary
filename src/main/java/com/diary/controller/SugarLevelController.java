50f1d48007dad7b58d94dfd5d792d4a08d5a047b
AndrewVasin <ironfeyd@gmail.com>
2022-06-01 22:38:06 +0300

    добавил валидацию в метод saveSugarLevelRecord контроллера

--- src/main/java/com/diary/controller/SugarLevelController.java
+++ src/main/java/com/diary/controller/SugarLevelController.java
@@ -5,10 +5,13 @@ import com.diary.service.SugarLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class SugarLevelController {
@@ -32,9 +35,14 @@ public class SugarLevelController {
     }
 
     @PostMapping("/saveSugarLevelRecord")
-    public String saveSugarLevelRecord(@ModelAttribute("sugarLevelRecord") SugarLevelRecord sugarLevelRecord) {
+    public String saveSugarLevelRecord(@Valid @ModelAttribute("sugarLevelRecord") SugarLevelRecord sugarLevelRecord,
+                                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new_record";
+        }
         // save sugarLevelRecord to database
         sugarLevelService.saveSugarLevelRecord(sugarLevelRecord);
+
         return "redirect:/";
     }
 
--- src/main/java/com/diary/model/SugarLevelRecord.java
+++ src/main/java/com/diary/model/SugarLevelRecord.java
@@ -2,10 +2,12 @@ package com.diary.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -18,20 +20,24 @@ public class SugarLevelRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NonNull
+    //@NonNull
+    @NotEmpty(message = "Пожалуйста, введите дату и время")
     // форматирование ввода даты для html-кода <input type="datetime-local"...
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @Column(name = "date_time")
     private LocalDateTime date;
 
-    @NonNull
+    @NotEmpty(message = "Пожалуйста, введите уровень сахара")
+    @Range(min = 0, max = 35, message = "Уровень сахара не может быть отрицательным или превышать 35 ммоль/л")
     @Column(name = "sugar_level")
     private Float sugarLevel;
 
-    @NonNull
+    @NotEmpty(message = "Пожалуйста, введите вид измерения")
+    @Size(min = 3, max = 35, message = "Пожалуйста, введите вид измерения (от 3 до 35 символов)")
     @Column(name = "measuring_type")
     private String measuringType;
 
+    @Size(max = 255, message = "Длина коментария не должна превышать 255 символов")
     @Column(name = "comment")
     private String comment;
 }
