ad4c778ce05d96d642c2605688449d251e3cd3ab
AndrewVasin <ironfeyd@gmail.com>
2022-06-03 09:59:33 +0300

    подключил favicon

--- src/main/java/com/diary/service/WebConfig.java
+++ src/main/java/com/diary/service/WebConfig.java
@@ -13,5 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("static/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("static/img/**").addResourceLocations("classpath:/static/img/");
     }
 }
